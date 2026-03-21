35285b71e51b5e3c9ed5fef8be38fc02d28266ed
rrekic <rrekic1@etf.unsa.ba>
2026-03-21 22:37:22 +0100

    Solve 3643

---
+++ 3643. Flip Square Submatrix Vertically/ans.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    vector<vector<int> > reverseSubmatrix(vector<vector<int> >& grid, int x,
+                                         int y, int k) {
+
+        for (int i = 0; i < k / 2; i++) {
+            for (int j = 0; j < k; j++) {
+                int temp = grid[x + i][y + j];
+                grid[x + i][y + j] = grid[k + x - i - 1][y + j];
+                grid[k - i + x - 1][y + j] = temp;
+            }
+        }
+
+        return grid;
+    }
+};
