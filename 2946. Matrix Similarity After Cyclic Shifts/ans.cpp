b44598932a137e70c06361837f32ddfbfabd55af
rrekic <rrekic1@etf.unsa.ba>
2026-03-27 23:11:49 +0100

    Solve 2946

---
+++ 2946. Matrix Similarity After Cyclic Shifts/ans.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+#include <vector>
+
+class Solution {
+public:
+    bool areSimilar(vector<vector<int> >& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+  
+        k = k % n;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 0) {
+            
+                    if (mat[i][j] != mat[i][(j + k) % n])
+                        return false;
+                } else {
+                    // od c++11 % radi opreciju rem
+                    if (mat[i][j] != mat[i][(j - k + n) % n])
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
