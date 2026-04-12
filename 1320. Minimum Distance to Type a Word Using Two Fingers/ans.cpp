fef1f6d269e292ff4da80866dd8f2aacc4389b83
rrekic <rrekic1@etf.unsa.ba>
2026-04-12 21:57:07 +0200

    Solve 1320

---
+++ 1320. Minimum Distance to Type a Word Using Two Fingers/ans.cpp
@@ -0,0 +1,47 @@
+using namespace std;
+#include<vector>
+#include<string>
+#include<climits>
+
+class Solution {
+public:
+    int dist(int a, int b) {
+        if (a == 26)
+            return 0; 
+        int x1 = a / 6, y1 = a % 6;
+        int x2 = b / 6, y2 = b % 6;
+        return abs(x1 - x2) + abs(y1 - y2);
+    }
+
+    int minimumDistance(string word) {
+        int n = word.size();
+
+        vector<vector<int> > dp(n, vector<int>(27, INT_MAX));
+
+        
+        dp[0][26] = 0; 
+
+        for (int i = 1; i < n; i++) {
+            int cur = word[i] - 'A';
+            int prev = word[i - 1] - 'A';
+
+            for (int j = 0; j <= 26; j++) {
+                if (dp[i - 1][j] == INT_MAX)
+                    continue;
+
+                // isti prst
+                dp[i][j] = min(dp[i][j], dp[i - 1][j] + dist(prev, cur));
+
+                // drugi 
+                dp[i][prev] = min(dp[i][prev], dp[i - 1][j] + dist(j, cur));
+            }
+        }
+
+        int ans = INT_MAX;
+        for (int j = 0; j <= 26; j++) {
+            ans = min(ans, dp[n - 1][j]);
+        }
+
+        return ans;
+    }
+};
